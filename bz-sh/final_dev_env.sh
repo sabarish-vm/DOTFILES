51846ee39eb2550eb6f3ee92f2250dcef808031f
sabarish-vm <sabarish.science@gmail.com>
2024-08-29 18:59:10 +0200

    better compatibility

--- bz-sh/final_dev_env.sh
+++ bz-sh/final_dev_env.sh
@@ -1,7 +1,6 @@
 case "${DEVICE_NAME}" in
 
-mac) source "$DOTFILES/bz-sh/devices/mac_final.sh" ;;
-cast*) cast_init ;;
-hummel*) hummel_init ;;
-
+mac) [ -f "$DOTFILES/bz-sh/devices/mac_final.sh" ] && source "$DOTFILES/bz-sh/devices/mac_final.sh" ;;
+cast*) [ -f "$DOTFILES/bz-sh/devices/cast_final.sh" ] && source "$DOTFILES/bz-sh/devices/cast_final.sh" ;;
+hummel*) [ -f "$DOTFILES/bz-sh/devices/hummel_final.sh" ] && source "$DOTFILES/bz-sh/devices/hummel_final.sh" ;;
 esac
