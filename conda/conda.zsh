3de668a71a2ccf59dec9cd038c34d23026e123d9
sabarish-vm <sabarish.science@gmail.com>
2024-08-31 19:28:34 +0200

    better conda init creation

--- conda/conda.zsh
+++ conda/conda.zsh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 if [ -f "$DOTFILES/conda/conda_init_zsh.gen" ] &&
-    [ grep '>>> conda init' "$DOTFILES/conda/conda_init_zsh.gen" ] ; then
-    source "$DOTFILES/conda/conda_init_zsh.gen"
+    grep '>>> conda init' "$DOTFILES/conda/conda_init_zsh.gen" > /dev/null 2>&1 ; then
+    . "$DOTFILES/conda/conda_init_zsh.gen"
 else 
     if [ -f "$DOTFILES/conda/conda_path.gen" ] ; then 
-        conda_bin="$(cat $DOTFILES/conda/conda_path.gen)"
-        contents="$($conda_bin init zsh -d -v)"
-        line_numbers="$(echo $contents | grep -n -e '>>>' -e '<<<' | cut -f 1 -d ':')"
-        l1="$(echo $line_numbers | sed -n 1p )"
-        l2="$(echo $line_numbers | sed -n 2p )"
+        conda_bin="$(head "$DOTFILES/conda/conda_path.gen" )"
+        contents="$("$conda_bin" init zsh -d -v)"
+        line_numbers="$(echo "$contents" | grep -n -e '>>>' -e '<<<' | cut -f 1 -d ':')"
+        l1="$(echo "$line_numbers" | sed -n 1p )"
+        l2="$(echo "$line_numbers" | sed -n 2p )"
         echo "${line_numbers}"
-        contents="$(echo $contents | sed -n ${l1},${l2}p | cut -c 2-)"
+        contents="$(echo "$contents" | sed -n "${l1}","${l2}p" | cut -c 2-)"
         echo "${contents}" > "$DOTFILES/conda/conda_init.zsh"
-        source "$DOTFILES/conda/conda_init.zsh"
-    else 
+        . "$DOTFILES/conda/conda_init.zsh"
+    else
         echo "Conda path was not setup during DOTFILES installation"
     fi
 fi
