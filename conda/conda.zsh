723777f0eb0a09c9bb8214fc1654e767b8436b4e
sabarish-vm <sabarish.science@gmail.com>
2024-08-31 19:40:38 +0200

    conda fix

--- conda/conda.zsh
+++ conda/conda.zsh
@@ -9,10 +9,9 @@ else
         line_numbers="$(echo "$contents" | grep -n -e '>>>' -e '<<<' | cut -f 1 -d ':')"
         l1="$(echo "$line_numbers" | sed -n 1p )"
         l2="$(echo "$line_numbers" | sed -n 2p )"
-        echo "${line_numbers}"
-        contents="$(echo "$contents" | sed -n "${l1}","${l2}p" | cut -c 2-)"
-        echo "${contents}" > "$DOTFILES/conda/conda_init.zsh"
-        . "$DOTFILES/conda/conda_init.zsh"
+        contents="$(echo "$contents" | sed -n "${l1}","${l2}"p | cut -c 2-)"
+        echo "${contents}" > "$DOTFILES/conda/conda_init_zsh.gen"
+        . "$DOTFILES/conda/conda_init_zsh.gen"
     else
         echo "Conda path was not setup during DOTFILES installation"
     fi
