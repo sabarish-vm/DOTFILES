da20543cff52419225a6563c130dd233c8ddeee3
sabarish-vm <sabarish.science@gmail.com>
2024-08-31 18:28:39 +0200

    better installer

--- install.sh
+++ install.sh
@@ -1,24 +1,58 @@
 #!/usr/bin/env sh
 
-source $HOME/.dotfiles/paths.sh
+[ -z "$DOTFILES" ] && { export DOTFILES="$HOME/.dotfiles"; }
+. "$DOTFILES/paths.sh"
 mkdir -p "$egetbin"
-
-[ -z "$DOTFILES" ] && { export DOTFILES="$HOME/.dotfiles" ; }
-
-source $DOTFILES/cli_tools/setup.sh
-
-condaexe=$(echo "$condaexe" | tr -d '[:space:]')
-[ -n "${condaexe}" ] && { echo "${condaexe}" > $DOTFILES/conda/conda_path.gen ; }
 
 link() {
     linkerFile="$1"
-    sourceFile="$(cat ${linkerFile} | awk -F '==' '{ printf $1 }')"
-    sourceFile="${sourceFile/\$DOTFILES/$DOTFILES}"
-    targetFile="$(cat ${linkerFile} | awk -F '==' '{ printf $2 }')"
-    targetFile="${targetFile/\$HOME/$HOME}"
+    sourceFile="$(head "${linkerFile}" | awk -F '==' '{ printf $1 }')"
+    sourceFile="$(echo "$sourceFile" | sed "s#\$DOTFILES#$DOTFILES#g")"
+    targetFile="$(head "${linkerFile}" | awk -F '==' '{ printf $2 }')"
+    targetFile="$(echo "$targetFile" | sed "s#\$HOME#$HOME#g")"
     var="ln -s ${sourceFile} ${targetFile}"
     echo "$var"
     eval "$var"
 }
 
-link "$DOTFILES/zsh/zsh.linker"
+cli_manager_ins() {
+    . "$DOTFILES/cli_tools/setup.sh"
+}
+
+conda_ins() {
+    condaexe=$(echo "$condaexe" | tr -d '[:space:]')
+    [ -n "${condaexe}" ] && { echo "${condaexe}" >"$DOTFILES/conda/conda_path.gen"; }
+}
+
+zsh_ins() {
+    link "$DOTFILES/zsh/zsh.linker"
+}
+
+all() {
+    cli_manager_ins
+    conda_ins
+    zsh_ins
+}
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    --conda)
+        conda_ins
+        shift 1
+        ;;
+    --cli_manager)
+        cli_manager_ins
+        shift 1
+        ;;
+    --zsh)
+        zsh_ins
+        shift 1
+        ;;
+    --all)
+        all
+        exit 0
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
+done
