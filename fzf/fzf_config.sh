3e8452c82b281f0ff3c69395e5b7c3627343210d
sabarish-vm <sabarish.science@gmail.com>
2024-09-01 17:19:47 +0200

    fzf-ctrl-t opts

--- fzf/fzf_config.sh
+++ fzf/fzf_config.sh
@@ -1,2 +1,4 @@
 export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="$(grep -e '^[^#]' $DOTFILES/fzf/fzf_ctrl_T_opts |
+    tr '\n' ' ')"
