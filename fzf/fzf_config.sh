fbe800ca86ae67c8096a4cb8bd5435bfefa5cb05
sabarish-vm <sabarish.science@gmail.com>
2024-09-06 09:51:56 +0200

    change fzf completion trigger

--- fzf/fzf_config.sh
+++ fzf/fzf_config.sh
@@ -1,3 +1,4 @@
+export FZF_COMPLETION_TRIGGER=',,'
 export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="$(grep -e '^[^#]' $DOTFILES/fzf/fzf_ctrl_T_opts |
