a1c271e0c2901ed0f66c8edefcfbe12af4f0aeed
sabarish-vm <sabarish.science@gmail.com>
2024-09-07 11:08:18 +0200

    add texocr alias
    add latex2ocr alias

--- bz-sh/devices/mac/mac_init.sh
+++ bz-sh/devices/mac/mac_init.sh
@@ -1,3 +1,5 @@
 export PATH="/opt/homebrew/bin/:$PATH"
 [ -f "$DOTFILES/bz-sh/devices/mac/mac_init.prv.sh" ] &&
     source "$DOTFILES/bz-sh/devices/mac/mac_init.prv.sh"
+
+alias texocr='PATH="/Users/sabarish/opt/miniconda3/envs/latexocr/bin/:$PATH" latexocr'
