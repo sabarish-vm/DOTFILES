d4346428674f75ed7db98cff2526e9f2482a9cae
sabarish-vm <sabarish.science@gmail.com>
2024-09-26 16:59:46 +0200

    adding hpc aliases

--- bz-sh/aliases.sh
+++ bz-sh/aliases.sh
@@ -39,3 +39,9 @@ alias cl='clear'
 alias gst='git status'
 alias br='broot'
 alias lg='lazygit'
+
+#################################### HPC stuff
+alias me="[[  0 != \$(squeue --me | tail -n +2 | wc -l) ]] && paste -d '         ' <(jobqueue) <( echo 'WorkDir'; jobqueue | tail -n +2 | awk '{print \$1}' | xargs -n 1 scontrol show job | rg 'WorkDir=(.*)' -or '\$1' | xargs realpath ) || echo 'No running jobs'"
+alias jobcancelz="(me) | tail -n +2 |fzf -m | awk '{print \$1}' | xargs -r -n 1 scancel"
+alias jobcd='cd $((me) | tail -n +2 |fzf | awk '\''{print $6}'\'')'
+alias jobdata='(me) | tail -n +2 |fzf -m | awk '\''{printf("%s""/data"),$6}'\'' | xargs -r -n 1 ls'
