42e1b12dc4e7fd3026916223656a5b7b6d830e71
sabarish-vm <sabarish.science@gmail.com>
2024-09-26 17:00:57 +0200

    mat2py formatting

--- bz-sh/commands.sh
+++ bz-sh/commands.sh
@@ -8,7 +8,11 @@ gitget() {
 }
 bookopen() { fd "$1" $HOME/Books/ | fzf --bind 'enter:become(open {})'; }
 gfd() { cd "$(fzf -e --bind 'enter:become(dirname {})')"; }
-mat2py() { python -c "from sympy.parsing.mathematica import parse_mathematica;expr=parse_mathematica(""'""${1}""'"") ;print();print(expr)"; }
+mat2py() {
+    python -c "from sympy.parsing.mathematica import parse_mathematica;expr=parse_mathematica(""'""${1}""'"") ;print();print(expr)"
+}
+
+###########################################################################################
 
 zja() {
     var=$(zellij ls | cut -c 8- | cut -d " " -f 1 | rev | cut -c 4- | rev | fzf)
@@ -22,3 +26,5 @@ zjk() {
     var=$(zellij ls | cut -c 8- | cut -d " " -f 1 | rev | cut -c 4- | rev | fzf)
     [[ -n "$var" ]] && zellij k $var
 }
+
+###########################################################################################
