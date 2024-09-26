e23ba0ac79377108b1851609841c437565e2316d
sabarish-vm <sabarish.science@gmail.com>
2024-09-26 17:20:55 +0200

    better path handling

--- paths.sh
+++
@@ -1,8 +0,0 @@
-## CANNOT BE CHANGED / ONLY FOR REFERENCE
-export opt="$HOME/opt"
-export optbin="$HOME/opt/bin"
-export dotfiles="$HOME/.dotfiles"
-export egetbin="$HOME/opt/bin/eget"
-
-## NEEDS TO BE PROVIDED
-export condaexe="$HOME/opt/miniconda3/bin/conda"
