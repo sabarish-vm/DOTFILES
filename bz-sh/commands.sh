e994ee4e6cfb8e7e64d75e49abc3c3241b078f86
sabarish-vm <sabarish.science@gmail.com>
2024-09-26 16:33:09 +0200

    renaminig fxns + new zellij fxns

--- bz-sh/commands.sh
+++ bz-sh/commands.sh
@@ -1,6 +1,5 @@
 ## Functions
-rawgithub() { echo $1 | sed 's/github/raw.githubusercontent/g' | sed 's#blob\/##g'; }
-githubdownload() {
+gitget() {
     if [ -n "$2" ]; then
         wget $(echo $1 | sed 's/github/raw.githubusercontent/g' | sed 's#blob\/##g') -O $2
     else
@@ -9,4 +8,17 @@ githubdownload() {
 }
 bookopen() { fd "$1" $HOME/Books/ | fzf --bind 'enter:become(open {})'; }
 gfd() { cd "$(fzf -e --bind 'enter:become(dirname {})')"; }
-mat2pyParser() { python -c "from sympy.parsing.mathematica import parse_mathematica;expr=parse_mathematica(""'""${1}""'"") ;print();print(expr)"; }
+mat2py() { python -c "from sympy.parsing.mathematica import parse_mathematica;expr=parse_mathematica(""'""${1}""'"") ;print();print(expr)"; }
+
+zja() {
+    var=$(zellij ls | cut -c 8- | cut -d " " -f 1 | rev | cut -c 4- | rev | fzf)
+    [[ -n "$var" ]] && zellij a $var
+}
+zjd() {
+    var=$(zellij ls | cut -c 8- | cut -d " " -f 1 | rev | cut -c 4- | rev | fzf)
+    [[ -n "$var" ]] && zellij d $var
+}
+zjk() {
+    var=$(zellij ls | cut -c 8- | cut -d " " -f 1 | rev | cut -c 4- | rev | fzf)
+    [[ -n "$var" ]] && zellij k $var
+}
