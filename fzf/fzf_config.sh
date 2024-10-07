12e8244d75ae3af68d1c38a9b9da22cf8578fa8d
sabarish-vm <sabarish.science@gmail.com>
2024-10-07 18:10:39 +0200

    toggle F/D HF/HD UF/UD

--- fzf/fzf_config.sh
+++ fzf/fzf_config.sh
@@ -7,25 +7,31 @@ export FZF_CTRL_T_OPTS="$(grep -e '^[^#]' $DOTFILES/fzf/fzf_ctrl_T_opts |
 _fzf_comprun() {
     local command=$1
     shift
-    rootdir="$(echo "$@" | sed 's#.*walker-root=\(.*\)#\1#g')"
+    dir="$(echo "$@" | sed 's#.*walker-root=\(.*\)#\1#g')"
     case "$command" in
     cd | z)
         fd -t d '.' "$dir" | fzf --prompt 'D> ' \
-            --bind 'ctrl-h:transform:[[ ! $FZF_PROMPT =~ H ]] &&
-        echo "change-prompt(HD> )+reload(fd -t d --hidden . $dir)" ||
-        echo "change-prompt(D> )+reload(fd -t d . $dir)"' \
+            --bind "ctrl-h:transform:[[ ! \$FZF_PROMPT =~ H ]] &&
+        echo 'change-prompt(HD> )+reload(fd -t d --hidden . $dir)' ||
+        echo 'change-prompt(D> )+reload(fd -t d . $dir)'" \
+            --bind "ctrl-i:transform:[[ ! \$FZF_PROMPT =~ U ]] &&
+        echo 'change-prompt(UD> )+reload(fd -t d --unrestricted . $dir)' ||
+        echo 'change-prompt(D> )+reload(fd -t d . $dir)'" \
             --preview \
             'eza --tree -L 1 --icons --group-directories-first --color=always {}'
         ;;
     *)
         fd -t f '.' "$dir" | fzf --prompt 'F> ' \
-            --bind 'ctrl-f:transform:[[ ! $FZF_PROMPT =~ F ]] &&
-            echo "change-prompt(F> )+reload(fd --type file)" ||
-            echo "change-prompt(D> )+reload(fd --type directory)"' \
-            --bind 'ctrl-h:transform:[[ ! $FZF_PROMPT =~ HF ]] && 
-            echo "change-prompt(HF> )+reload(fd --type file --hidden)" ||
-            echo "change-prompt(HD> )+reload(fd --type directory --hidden)"' \
-            --preview '[[ $FZF_PROMPT =~ "HF|F" ]] &&
+            --bind "ctrl-f:transform:[[ ! \$FZF_PROMPT =~ F ]] &&
+            echo 'change-prompt(F> )+reload(fd --type file . $dir)' ||
+            echo 'change-prompt(D> )+reload(fd --type directory . $dir)'" \
+            --bind "ctrl-h:transform:[[ ! \$FZF_PROMPT =~ HF ]] && 
+            echo 'change-prompt(HF> )+reload(fd --type file --hidden . $dir)' ||
+            echo 'change-prompt(HD> )+reload(fd --type directory --hidden . $dir)'" \
+            --bind "ctrl-i:transform:[[ ! \$FZF_PROMPT =~ UF ]] && 
+            echo 'change-prompt(UF> )+reload(fd --type file --unrestricted . $dir)' ||
+            echo 'change-prompt(UD> )+reload(fd --type directory --unrestricted . $dir)'" \
+            --preview '[[ $FZF_PROMPT =~ "HF|UF|F" ]] &&
             bat --color=always {} ||
             eza --tree -L 1 --icons --color=always  {}'
         ;;
