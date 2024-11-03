06cf92fcd89322a2002983749b65171ffe72a79c
sabarish-vm <sabarish.science@gmail.com>
2024-11-03 13:58:50 +0100

    more fzf opts

--- fzf/fzf_config.sh
+++ fzf/fzf_config.sh
@@ -9,28 +9,44 @@ _fzf_comprun() {
     shift
     dir="$(echo "$@" | sed 's#.*walker-root=\(.*\)#\1#g')"
     case "$command" in
-    cd | z)
-        fd -t d '.' "$dir" | fzf --prompt 'D> ' \
+
+    ssh)
+        $DOTFILES/fzf/_fzf_ssh | fzf --preview 'awk -v var={} "{
+        slicedStr=substr(var,1,length(var)-1); pattern=\"^Host \" slicedStr \".*$\" ;if(match(\$0,pattern)) { inside=\"1\";print;next }
+        if(inside) {
+                if(match(\$0,/^Host [A-Za-z0-9]/)) {inside=\"\";next}
+                else {print;next}
+        }
+        }" $HOME/.ssh/config'
+        ;;
+
+    cd | z | mkdir)
+        fd -t d '.' "$dir" | fzf --prompt 'D>L2' \
             --bind "ctrl-h:transform:[[ ! \$FZF_PROMPT =~ H ]] &&
-        echo 'change-prompt(HD> )+reload(fd -t d --hidden . $dir)' ||
-        echo 'change-prompt(D> )+reload(fd -t d . $dir)'" \
+        echo 'change-prompt(HD>L2)+reload(fd -t d --hidden . $dir)' ||
+        echo 'change-prompt(D>L2)+reload(fd -t d . $dir)'" \
             --bind "ctrl-i:transform:[[ ! \$FZF_PROMPT =~ U ]] &&
-        echo 'change-prompt(UD> )+reload(fd -t d --unrestricted . $dir)' ||
-        echo 'change-prompt(D> )+reload(fd -t d . $dir)'" \
+        echo 'change-prompt(UD>L2)+reload(fd -t d --unrestricted . $dir)' ||
+        echo 'change-prompt(D>L2)+reload(fd -t d . $dir)'" \
+            --bind "shift-up:transform:$HOME/.dotfiles/fzf/fzf_level \$FZF_PROMPT up" \
+            --bind "shift-down:transform:$HOME/.dotfiles/fzf/fzf_level \$FZF_PROMPT down" \
             --preview \
             'eza --tree -L 1 --icons --group-directories-first --color=always {}'
         ;;
+
     *)
-        fd -t f '.' "$dir" | fzf --prompt 'F> ' \
+        fd -t f '.' "$dir" | fzf --prompt 'F>L2 ' \
             --bind "ctrl-f:transform:[[ ! \$FZF_PROMPT =~ F ]] &&
-            echo 'change-prompt(F> )+reload(fd --type file . $dir)' ||
-            echo 'change-prompt(D> )+reload(fd --type directory . $dir)'" \
+            echo 'change-prompt(F>L2 )+reload(fd --type file . $dir)' ||
+            echo 'change-prompt(D>L2 )+reload(fd --type directory . $dir)'" \
             --bind "ctrl-h:transform:[[ ! \$FZF_PROMPT =~ HF ]] && 
-            echo 'change-prompt(HF> )+reload(fd --type file --hidden . $dir)' ||
-            echo 'change-prompt(HD> )+reload(fd --type directory --hidden . $dir)'" \
+            echo 'change-prompt(HF>L2 )+reload(fd --type file --hidden . $dir)' ||
+            echo 'change-prompt(HD>L2 )+reload(fd --type directory --hidden . $dir)'" \
             --bind "ctrl-i:transform:[[ ! \$FZF_PROMPT =~ UF ]] && 
-            echo 'change-prompt(UF> )+reload(fd --type file --unrestricted . $dir)' ||
-            echo 'change-prompt(UD> )+reload(fd --type directory --unrestricted . $dir)'" \
+            echo 'change-prompt(UF>L2 )+reload(fd --type file --unrestricted . $dir)' ||
+            echo 'change-prompt(UD>L2 )+reload(fd --type directory --unrestricted . $dir)'" \
+            --bind "shift-up:transform:$HOME/.dotfiles/fzf/fzf_level \$FZF_PROMPT up" \
+            --bind "shift-down:transform:$HOME/.dotfiles/fzf/fzf_level \$FZF_PROMPT down" \
             --preview '[[ $FZF_PROMPT =~ "HF|UF|F" ]] &&
             bat --color=always {} ||
             eza --tree -L 1 --icons --color=always  {}'
