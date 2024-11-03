43b84e899749c8723b5737b1ef115b66a5e069f7
sabarish-vm <sabarish.science@gmail.com>
2024-11-03 15:24:40 +0100

    remove rtt dependency and use zoxide

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,9 @@
 fzf_recent_path() {
-    file=$(rtt)
-    LBUFFER+="$file"
+    file="$(zoxide query -l |\
+        fzf --reverse --height=100% --border=sharp --wrap --gap\
+        --preview 'command eza -D --tree -L 1 --color=always {} || command -p ls -Cp --color=always {}'\
+        --info=inline)"
+    LBUFFER+=" $file"
+    # zle zvm_exit_insert_mode
 }
 zle -N fzf_recent_path
