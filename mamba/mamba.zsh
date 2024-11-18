10effcd77c897e7df809c5cb4446adf330d238e6
sabarish-vm <sabarish.science@gmail.com>
2024-11-18 10:00:37 +0100

    check mamba in bin or not

--- mamba/mamba.zsh
+++ mamba/mamba.zsh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
+if command -v micromamba &>/dev/null ; then 
+    : 
+else 
+    echo "Micromamba not in path"
+fi
+
 if [ -f "$DOTFILES/mamba/mamba_init_zsh.gen" ] &&
     grep '>>>' "$DOTFILES/mamba/mamba_init_zsh.gen" > /dev/null 2>&1 ; then
     . "$DOTFILES/mamba/mamba_init_zsh.gen"
 else
-    if [ -f "$DOTFILES/mamba/mamba_path.gen" ] ; then 
-        mamba_path="$(head "$DOTFILES/mamba/mamba_path.gen" )"
-        contents="$(micromamba shell init -s zsh --dry-run $mamba_path)"
+    if [ -n "$DOT_MAMBA_DIR" ] ; then 
+        contents="$(micromamba shell init -s zsh --dry-run $DOT_MAMBA_DIR )"
         line_numbers="$(printf "%s" "$contents" | grep -n -e '>>>' -e '<<<' | cut -f 1 -d ':')"
         l1="$(printf "%s" "$line_numbers" | sed -n 1p )"
         l2="$(printf "%s" "$line_numbers" | sed -n 2p )"
@@ -13,7 +18,7 @@ else
         printf "%s" "${contents}" > "$DOTFILES/mamba/mamba_init_zsh.gen"
         . "$DOTFILES/mamba/mamba_init_zsh.gen"
     else
-        printf "%s" "mamba path was not setup during DOTFILES installation"
+        printf "Env variable %s %s" "DOT_MAMBA_DIR" " is not set in $DOTFILES/paths.sh"
     fi
 fi
 
