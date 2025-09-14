1cd64ab57329f30705ed8e61563de3097c133385
sabarish-vm <sabarish.science@gmail.com>
2025-09-14 17:31:10 +0200

    better PATH management

--- bz-sh/devices/mac/mac_init.sh
+++ bz-sh/devices/mac/mac_init.sh
@@ -1,8 +1,6 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
-export PATH="/opt/homebrew/bin:$PATH"
+source "$DOTFILES/bz-sh/devices/mac/mac_paths.sh"
 [ -f "$DOTFILES/bz-sh/devices/mac/mac_init.prv.sh" ] &&
     source "$DOTFILES/bz-sh/devices/mac/mac_init.prv.sh"
 
 alias texocr='PATH="/Users/sabarish/opt/micromamba/envs/latexocr/bin/:$PATH" latexocr'
-alias sowcal='gcalcli --calendar Work'
-alias mecal='gcalcli --calendar Sabarish'
---
+++ bz-sh/devices/mac/mac_paths.sh
@@ -0,0 +1,7 @@
+paths="
+/opt/homebrew/bin/
+$HOME/.cargo/bin/
+"
+
+stacked="$($DOTFILES/bz-sh/path_gen ${paths})"
+export PATH="$stacked$PATH"
