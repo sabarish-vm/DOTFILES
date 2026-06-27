7cea6ffda49b9f45a5af71a70f5045e5145a2e25
sabarish-vm <sabarish.science@gmail.com>
2026-06-27 18:17:53 +0200

    update keymaps in wezterm

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -4,17 +4,17 @@ local M = {}
 
 function M.apply_keymap(config)
 	config.leader = { key = "n", mods = "CTRL", timeout_milliseconds = 1000 }
-	table.insert(config.keys, { key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay })
-	table.insert(config.keys, {
-		key = "LeftArrow",
-		mods = "CTRL|SHIFT",
-		action = act.SendKey({ key = "LeftArrow", mods = "CTRL|SHIFT" }),
-	})
-	table.insert(config.keys, {
-		key = "RightArrow",
-		mods = "CTRL|SHIFT",
-		action = act.SendKey({ key = "RightArrow", mods = "CTRL|SHIFT" }),
-	})
+
+	local keymap_definitions = {
+		{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
+		{ key = "LeftArrow", mods = "CTRL|SHIFT", action = act.SendKey({ key = "LeftArrow", mods = "CTRL|SHIFT" }) },
+		{ key = "RightArrow", mods = "CTRL|SHIFT", action = act.SendKey({ key = "RightArrow", mods = "CTRL|SHIFT" }) },
+		{ key = "l", mods = "SUPER|SHIFT", action = act.MoveTabRelative(1) },
+		{ key = "h", mods = "SUPER|SHIFT", action = act.MoveTabRelative(-1) },
+	}
+	for _, binding in ipairs(keymap_definitions) do
+		table.insert(config.keys, binding)
+	end
 end
 
 return M
