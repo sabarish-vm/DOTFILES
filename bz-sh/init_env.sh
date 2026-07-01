1e650941fc0c767bbad4b32f2f44ac485af421c0
sabarish-vm <sabarish.science@gmail.com>
2026-07-01 09:06:41 +0200

    better path exporting

--- bz-sh/init_env.sh
+++ bz-sh/init_env.sh
@@ -1,10 +1,11 @@
 ## THIS FILE CONTAINS ONLY IMPORTANT EXPORTS
 . $HOME/.dotfiles/paths.sh
 export LANG="en_US.UTF-8"
-export PATH="$DOT_OPTBIN:$PATH"
-export PATH="$BINMGR_BIN:$EGET_BIN:$PATH"
-
-[ -n "${DOT_BREW_HOME}" ] && { eval "$($DOT_BREW_HOME/bin/brew shellenv)"; }
+[ -n "$DOT_OPTBIN" ] && export PATH="$DOT_OPTBIN:$PATH"
+[ -n "$EGET_BIN" ] && export PATH="$EGET_BIN:$PATH"
+[ -n "$BINMGR_BIN" ] && export PATH="$BINMGR_BIN:$PATH"
+[ -n "${DOT_BREW_HOME}" ] && eval "$($DOT_BREW_HOME/bin/brew shellenv)"
+[ -n "${PIXI_HOME}" ] && export PATH="$PIXI_HOME/bin:$PATH"
 
 devname=$(cat "$HOME/.device_name") &>/dev/null && export DEVICE_NAME="$devname" ||
     {
