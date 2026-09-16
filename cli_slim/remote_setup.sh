c86799dbda4ef1c4dcbb64f7d1a232e61f91d44c
sabarish-vm <sabarish.science@gmail.com>
2026-09-16 19:57:52 +0200

    cli_slim : add the shell script

---
+++ cli_slim/remote_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install PIXI
+export PIXI_NO_PATH_UPDATE=1
+curl -fsSL https://pixi.sh/install.sh | sh
+ACTUAL_PIXI_HOME="${PIXI_HOME:-$HOME/.pixi}"
+echo "export PATH=$ACTUAL_PIXI_HOME/bin:\$PATH" >>"$HOME/.bashrc"
+export PATH="$ACTUAL_PIXI_HOME/bin:$PATH"
+mkdir -p "$ACTUAL_PIXI_HOME/manifests/"
+
+# Get the pixi-global.toml from somewhere
+curl -sL raw.githubusercontent.com/sabarish-vm/dotfiles/main/cli_slim/pixi-global.toml >"$ACTUAL_PIXI_HOME/manifests/pixi-global.toml"
+pixi g sync
+
+# Set up the CLI tools
+echo 'eval "$(fzf --bash)"' >>"$HOME/.bashrc"
+echo 'eval "$(zoxide init bash)"' >>"$HOME/.bashrc"
