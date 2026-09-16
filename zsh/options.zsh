5f959a628d35c239306c2a22e2ff4f696d5f606e
sabarish-vm <sabarish.science@gmail.com>
2026-09-16 11:34:15 +0200

    zsh: better history options

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,6 +1,15 @@
+# History options
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+setopt HIST_EXPIRE_DUPS_FIRST # Delete oldest duplicate commands first when history fills up
+setopt HIST_IGNORE_DUPS       # Do not write a command to history if it matches the previous one
+setopt HIST_IGNORE_SPACE      # Ignore commands starting with a space (great for hiding passwords)
+setopt HIST_REDUCE_BLANKS     # Remove extra wide spaces from commands before saving
+setopt HIST_VERIFY
+setopt EXTENDED_HISTORY
 setopt SHARE_HISTORY
+
+# Other options
 unsetopt beep
 setopt extendedglob
